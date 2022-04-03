3f0eeed93fe511603fad52a0136b8cdbcca0888f
Shadi <shadialaghbari@gmail.com>
2022-04-03 04:27:43 +0200

    fixed docuemnt not changing to ltr

--- src/hooks/useCursorPos.js
+++ src/hooks/useCursorPos.js
@@ -2,26 +2,25 @@ import { useEffect, useState } from "react";
 import useLanguageInfo from "./useLanguageInfo";
 
 const useCursorPos = () => {
-    const [cursorCoordinates, setCursorCoordinates] = useState({
-        x: "",
-        y: "",
-    });
+	const [cursorCoordinates, setCursorCoordinates] = useState({
+		x: "",
+		y: "",
+	});
 
-    const { siteDir, siteLang, isRTL } = useLanguageInfo();
-    useEffect(() => {
-        document.documentElement.dir = "rtl";
-        function handleOnMouseMove(e) {
-            const x = e.offsetX;
-            const y = e.offsetY;
+	const { siteDir, siteLang, isRTL } = useLanguageInfo();
+	useEffect(() => {
+		function handleOnMouseMove(e) {
+			const x = e.offsetX;
+			const y = e.offsetY;
 
-            setCursorCoordinates({ x, y });
-        }
-        document.addEventListener("mousemove", handleOnMouseMove);
-        return () => {
-            document.removeEventListener("mousemove", handleOnMouseMove);
-        };
-    }, [siteDir]);
-    return { siteLang, siteDir, cursorCoordinates, isRTL };
+			setCursorCoordinates({ x, y });
+		}
+		document.addEventListener("mousemove", handleOnMouseMove);
+		return () => {
+			document.removeEventListener("mousemove", handleOnMouseMove);
+		};
+	}, [siteDir]);
+	return { siteLang, siteDir, cursorCoordinates, isRTL };
 };
 
 export default useCursorPos;
